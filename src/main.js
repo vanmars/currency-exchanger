3090d36edd439f39336269030f1b763a96d92c30
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-11 09:18:29 -0700

    Build document ready function with commented plan of action

--- src/main.js
+++ src/main.js
@@ -2,3 +2,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyService from './currencyService.js'
+
+$(document).ready(function(){
+  $().submit(function(event){
+    event.preventDefault();
+    // Grab User Inputs and Store in Variables
+    // Clear Input Fields
+    // Write getElements function
+    // Run getCurrency Method that feeds into getElements funtion
+  
+  })
+});
