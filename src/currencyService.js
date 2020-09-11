820d314ff4b4b9195de0d2609632648c4c037fc8
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-11 09:17:45 -0700

    Fix typo

--- src/currencyService.js
+++ src/currencyService.js
@@ -1,4 +1,4 @@
-export default class currencyService {
+export default class CurrencyService {
   static getConvertedAmount () {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
     .then(function(response){
