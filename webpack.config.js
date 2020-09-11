3e81d749a8106b2501c53f9a6e96151a36e77916
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-11 08:19:57 -0700

    Build initial file structure

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'project',
+      title: 'currecny-exchanger',
       template: './src/index.html',
       inject: 'body'
     }),
