24b2a74d8fa40db1fae3290dd312acac15d006a2
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-11 11:34:29 -0700

    Refactor code by eliminating commented out code

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from './currencyService.js';
 
-// Write getElements function
 function getElements(response, inputtedAmount, inputtedCurrency){
   if(response.conversion_rates){
     let rate = response.conversion_rates[inputtedCurrency];
@@ -14,22 +13,17 @@ function getElements(response, inputtedAmount, inputtedCurrency){
       $(".showResult").text(`${inputtedAmount} USD is ${rate*inputtedAmount} ${inputtedCurrency}.`);
     }
   } else {
-    $(".showError").text(`An error occured: ${response.error}`);
+    $(".showError").text(`An error occured: ${response.message}`);
   }
 }
 
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    // Grab User Inputs and Store in Variables
     const inputtedAmount = $("#amount").val();
     const inputtedCurrency = $("#currency").val();
-    // const inputtedCurrencyName = $("#currency").name();
-    // Clear Input Fields
     $("#amount").val("");
     $("#currency").val("");
-    // $("#currency").name("");
-    // Run getCurrency Method that feeds into getElements funtion
     CurrencyService.getConvertedAmount()
       .then(function(response){
         getElements(response, inputtedAmount, inputtedCurrency);
