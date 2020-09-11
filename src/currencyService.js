8881ef1241bfbd7a7c79e71bce798bfb0c6f88c6
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-11 10:22:44 -0700

    Fix errors

--- src/currencyService.js
+++ src/currencyService.js
@@ -1,14 +1,14 @@
 export default class CurrencyService {
   static getConvertedAmount () {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-    .then(function(response){
-      if (!response.ok) {
-        throw Error(statusText);
-      }
-      return response.json();
-    })
-    .catch(function(error){
-      return error;
-    })
-  };
-};
+      .then(function(response){
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error){
+        return error;
+      });
+  }
+}
