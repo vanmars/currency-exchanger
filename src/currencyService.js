afc020b021ab68fd9676fc4fe65007a6ab5f4401
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-11 09:13:56 -0700

    Build service logic with currencyService class with static method and API fetch

---
+++ src/currencyService.js
@@ -0,0 +1,14 @@
+export default class currencyService {
+  static getConvertedAmount () {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+    .then(function(response){
+      if (!response.ok) {
+        throw Error(statusText);
+      }
+      return response.json();
+    })
+    .catch(function(error){
+      return error;
+    })
+  };
+};
