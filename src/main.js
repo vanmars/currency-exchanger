a2fca4084f63c407162f7e4b5595aed2cdca5925
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-12 19:04:54 -0700

    Eliminate commented out code for two cards

--- src/main.js
+++ src/main.js
@@ -4,32 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from './currencyService.js';
 
-// function getCurrencyElements(response, inputtedAmount, inputtedCurrency){
-//   if(response.conversion_rates){
-//     let rate = response.conversion_rates[inputtedCurrency];
-//     if (inputtedCurrency === "MAL"){
-//       $(".showCurrencyResult").text("Sorry, the Malawian Kwacha is not supported at this time.");
-//     } else {
-//       $(".showCurrencyResult").html(`${inputtedAmount} USD = <span class='text-info'>${rate*inputtedAmount}</span> ${inputtedCurrency}`);
-//     }
-//   } else {
-//     $(".showCurrencyError").text(`An error occured: ${response.message}`);
-//   }
-// }
-
-// function getUSDElements(response, inputtedAmount, inputtedCurrency){
-//   if(response.conversion_rates){
-//     let rate = response.conversion_rates[inputtedCurrency];
-//     if (inputtedCurrency === "MAL"){
-//       $(".showUSDResult").text("Sorry, the Malawian Kwacha is not supported at this time.");
-//     } else {
-//       $(".showUSDResult").html(`${inputtedAmount} ${inputtedCurrency} = <span class='text-info'>${inputtedAmount/rate}</span> USD`);
-//     }
-//   } else {
-//     $(".showUSDError").text(`An error occured: ${response.message}`);
-//   }
-// }
-
 function getElements(response, inputtedAmount, inputtedBase, inputtedTarget){
   if(response.conversion_rates){
     let baseRate = response.conversion_rates[inputtedBase];
@@ -44,6 +18,7 @@ function getElements(response, inputtedAmount, inputtedBase, inputtedTarget){
   }
 }
 
+// Title Animations
 var textWrapper = document.querySelector('.ml12');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
@@ -74,23 +49,10 @@ $(document).ready(function(){
 
       $("form#converter").submit(function(event){
         event.preventDefault();
-        event.preventDefault();
         const inputtedAmount = $("#amount").val();
         const inputtedBase = $("#baseCurrency").val();
         const inputtedTarget = $("#targetCurrency").val();
         getElements(response, inputtedAmount, inputtedBase, inputtedTarget);
       });
-      // $("form#fromUSD").submit(function(event){
-      //   event.preventDefault();
-      //   const inputtedAmount = $("#amountFromUSD").val();
-      //   const inputtedCurrency = $("#currencyFromUSD").val();
-      //   getCurrencyElements(response, inputtedAmount, inputtedCurrency);
-      // });
-      // $("form#toUSD").submit(function(event){
-      //   event.preventDefault();
-      //   const inputtedAmount = $("#amountToUSD").val();
-      //   const inputtedCurrency = $("#currencyToUSD").val();
-      //   getUSDElements(response, inputtedAmount, inputtedCurrency);
-      // });
     });
 });
